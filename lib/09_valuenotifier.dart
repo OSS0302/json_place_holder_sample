0356cc7bf9b733d04a18301bcdb9c3a52da35010
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-09-17 12:33:34 +0900

    09_valuenotifier

---
+++ lib/09_valuenotifier.dart
@@ -0,0 +1,99 @@
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:json_place_holder_sample/data/02_http_api.dart';
+
+class ModelSetState extends StatefulWidget {
+  const ModelSetState({super.key});
+
+  @override
+  State<ModelSetState> createState() =>
+      _ModelSetStateState();
+}
+
+class _ModelSetStateState
+    extends State<ModelSetState> {
+  final httpSampleModel =  HttpSampleModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HttpSampleScreen'),
+      ),
+      body: Center(
+        child: FutureBuilder<String>(
+            future: getData(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
+                final jsonString = snapshot.data!;
+                final jsonMap = jsonDecode(jsonString) as Map<String, dynamic>;
+
+              }
+              return Text('title ${httpSampleModel.state.title} : body : ${httpSampleModel.state.body}');
+            }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        getData().then((value) {
+          setState(() {
+
+
+           httpSampleModel.fetchData();
+          });
+        });
+      }),
+    );
+  }
+}
+
+// Model (상태 & 로직)
+class HttpSampleModel extends ValueNotifier<HttpSampleState> {
+  // State (Value)
+  HttpSampleState _state = HttpSampleState();
+
+  HttpSampleState get state => _state;
+
+  HttpSampleModel() : super(HttpSampleState()) {
+    fetchData();
+  }
+
+
+
+  void fetchData() async {
+    final jsonString = await getData();
+
+    final jsonMap = jsonDecode(jsonString) as Map;
+
+    // 상태 변경
+    value = value.copyWith(
+      title: jsonMap['title'],
+      body: jsonMap['body'],
+    );
+
+    // 외부에 알려주자
+    notifyListeners();
+  }
+}
+
+// State
+class HttpSampleState {
+  final String title;
+  final String body;
+
+  HttpSampleState({
+    this.title = '',
+    this.body = 'Loading',
+  });
+
+  HttpSampleState copyWith({
+    String? title,
+    String? body,
+  }) {
+    return HttpSampleState(
+      title: title ?? this.title,
+      body: body ?? this.body,
+    );
+  }
+}
